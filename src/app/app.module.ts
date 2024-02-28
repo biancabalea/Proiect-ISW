008a35df27e045fa90049f441be7b8bbca48cdc2
Monica Anghel <126202464+12Monica@users.noreply.github.com>
2024-02-28 20:59:45 +0200

    varianta 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { registerLocaleData } from "@angular/common";
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./auth/components/login/login.component";
+import { SignupComponent } from "./auth/components/signup/signup.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { NgModule } from "@angular/core";
+
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    NzSpinModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule 
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
