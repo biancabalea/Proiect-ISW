57e7760d9493af3c1810a687f317132850f99b74
biancabalea <126229336+biancabalea@users.noreply.github.com>
2024-02-29 20:47:11 +0200

    v3 frontend
    erau cateva erori in v2, am verificat si modificat erorile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { registerLocaleData } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from "./app.component";
-import { LoginComponent } from "./auth/components/login/login.component";
-import { SignupComponent } from "./auth/components/signup/signup.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 
 registerLocaleData(en);
 
@@ -40,6 +42,7 @@ registerLocaleData(en);
     NzLayoutModule 
   ],
   providers: [
+    provideClientHydration(),
     { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [AppComponent]
