c66651c29f19291e792f736658680d97063a0ba3
Monica Anghel <126202464+12Monica@users.noreply.github.com>
2024-03-02 16:03:35 +0200

    v4

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import {NzMessageService} from "ng-zorro-antd/message"
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -11,7 +15,10 @@ export class SignupComponent {
   isSpinning: boolean = false;
   signupForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -34,5 +41,15 @@ export class SignupComponent {
 
   register() {
     console.log(this.signupForm.value);
-  }
+    this.authService.register(this.signupForm.value).subscribe((res) => {
+      console.log(res);
+      if(res.id != null) {
+        this.message.success("Signup successful", {nzDuration: 5000});
+        this.router.navigateByUrl("/login");
+      } else {
+        this.message.error("Something went wrong", {nzDuration:5000});
+      }
+      })
+    }
+  
 }
