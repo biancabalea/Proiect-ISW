7b82ab8f805acf7af05a2626c323143c0277d21f
Monica Anghel <126202464+12Monica@users.noreply.github.com>
2024-03-03 20:09:28 +0200

    v6

--- src/app/auth/services/storage/storage.service.ts
+++ src/app/auth/services/storage/storage.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { stringify } from 'node:querystring';
+
+const TOKEN = "token";
+const USER = "user";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,46 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  static saveToken(token:string): void{
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.setItem(TOKEN, token);
+  }
+
+  static saveUser(user:any): void{
+    window.localStorage.removeItem(USER);
+    window.localStorage.setItem(USER, JSON.stringify(user));
+  }
+
+  static getToken(){
+    return window.localStorage.getItem(TOKEN);
+  }
+
+  static getUser() {
+    const userString = localStorage.getItem(USER);
+  
+    if (userString === null) {
+      return null;
+    }
+  
+    return JSON.parse(userString);
+  }
+
+  static getUserRole():string{
+    const user = this.getUser();
+    if (user == null) return "";
+    return user.role;
+  }
+
+  static isAdminLoggedIn():boolean{
+    if(this.getToken() == null) return false;
+    const role : string = this.getUserRole();
+    return role == "ADMIN";
+  }
+
+  static isCustomerLoggedIn(): boolean{
+    if(this.getToken() == null) return false;
+    const role: string = this.getUserRole();
+    return role == "CUSTOMER";
+  }
 }
